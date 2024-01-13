f6269d6eb6fa152980d766bec63721f960a215b7
Nhan Vo Nguyen (2585) <nhanvonguyen@kms.com.vn>
2024-01-14 00:07:39 +0700

    Build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4LTLPENU.mjs";import"./chunk-Q2MZJID6.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-AHJ5IGMP.mjs";import"./chunk-Q2MZJID6.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/shared/app.const.ts
+++ src/app/shared/app.const.ts
@@ -1,4 +1,4 @@
-export const imagePath = '/assets/images';
+export const imagePath = 'assets/images';
 export const aosImageDuration = 1300;
 export const aosImageOffset = 120;
 export const typingTexts = ['daily lives', 'change', 'all people'];
